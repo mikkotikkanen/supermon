a804910e443fefd4fee6c87b6ff2842ebd5cb54a
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2020-01-12 17:42:40 +0200

    cleanup

--- src/lib/utils/debounce.ts
+++ src/lib/utils/debounce.ts
@@ -2,8 +2,8 @@ export default (func: Function, delay: number) => {
   let inDebounce: NodeJS.Timeout;
 
   return function () {
-    const args = arguments
-    clearTimeout(inDebounce)
-    inDebounce = setTimeout(() => func.apply(null, args), delay)
+    const args = arguments;
+    clearTimeout(inDebounce);
+    inDebounce = setTimeout(() => func.apply(null, args), delay);
   }
 }
