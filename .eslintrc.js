b26d6aba63a3f53a25b219a6959e0f4e29dbc5cc
Mikko Tikkanen <mikko.tikkanen@aller.com>
2020-01-13 11:22:07 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',  // Allows for the use of imports
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'no-console': 'off',
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+  ],
+};
