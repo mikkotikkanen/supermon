21acee1884859dc514c458ee40e1ff9fcd75234d
Mikko Tikkanen <mikko.tikkanen@aller.com>
2020-01-13 11:22:13 +0200

    eslint fixes

--- tests/apps/test-server.js
+++ tests/apps/test-server.js
@@ -1,19 +1,20 @@
-const http = require('http')
-const port = 3001
+const http = require('http');
+
+const port = 3001;
 
 const requestHandler = (request, response) => {
-  console.log(`GET ${request.url}`)
-  response.end('Hello Node.js Server')
-}
+  console.log(`GET ${request.url}`);
+  response.end('Hello Node.js Server');
+};
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    console.log('something bad happened', err);
+  } else {
+    console.log(`server is listening on ${port}`);
   }
-
-  console.log(`server is listening on ${port}`)
-})
+});
 
 process.on('close', server.close);
