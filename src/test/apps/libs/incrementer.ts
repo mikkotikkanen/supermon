76cb01e3b4f77c921b699af4a90494e3892a4900
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2020-01-27 22:23:45 +0200

    fix: resolved case where file paths werent mapping correctly with absolute paths

--- src/test/apps/libs/incrementer.ts
+++ src/test/apps/libs/incrementer.ts
@@ -1,20 +1,20 @@
 import {
   writeFileSync, readFileSync, unlinkSync, existsSync,
 } from 'fs';
-import { join } from 'path';
+import { resolve } from 'path';
 import getWorkDir from '../../lib/getWorkDir';
 
 const workDir = getWorkDir();
 
 export const clean = (filename: string): void => {
-  const filePath = join(workDir, filename);
+  const filePath = resolve(workDir, filename);
   if (existsSync(filePath)) {
     unlinkSync(filePath);
   }
 };
 
 export const getValue = (filename: string): number => {
-  const filePath = join(workDir, filename);
+  const filePath = resolve(workDir, filename);
   let value = -1;
   if (existsSync(filePath)) {
     value = parseInt(readFileSync(filePath, { encoding: 'utf8' }), 10);
@@ -24,7 +24,7 @@ export const getValue = (filename: string): number => {
 };
 
 export const incrementer = (filename: string): void => {
-  const filePath = join(workDir, filename);
+  const filePath = resolve(workDir, filename);
   const value = getValue(filename);
   writeFileSync(filePath, value + 1, { encoding: 'utf8' });
 };
