8bac2840c79f443748bbbe9edb3a4c3e8d709a72
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2020-01-27 22:33:56 +0200

    Basic restar test, fails through npm for some reason

--- src/test/restart.test.ts
+++ src/test/restart.test.ts
@@ -1,24 +1,53 @@
-// import { writeFileSync } from 'fs';
-// import { join } from 'path';
-// import { clean, getValue } from './apps/libs/incrementer';
-// import getWorkDir from './lib/getWorkDir';
-// import lib from '../lib';
+import { writeFileSync } from 'fs';
+import { join, resolve as pathResolve } from 'path';
+import { EventEmitter } from 'events';
+import { clean, getValue } from './apps/libs/incrementer';
+import getWorkDir from './lib/getWorkDir';
+import lib from '../lib';
 
-// const incrementFile = 'restart_incrementer';
-// const workDir = getWorkDir();
-// const touchFile = join(workDir, 'empty_touch_file.js');
+const workDir = getWorkDir();
+const appFile = join(workDir, '..', 'dist', 'test', 'apps', 'test-incrementer.js');
+const incrementFile = 'restart_incrementer';
+const touchFile = join(workDir, 'empty_touch_file.js');
+let isStarted = false;
 
 
-// test('restart', () => {
-//   clean(incrementFile);
+let events: EventEmitter;
 
-//   lib({
-//     executable: `../../dist/test/apps/restartIncrementer.js ${incrementFile}`,
-//     watchDir: '../../tmp',
-//   });
+beforeAll(() => {
+  clean(incrementFile);
+});
 
-//   // touch file
-//   writeFileSync(touchFile, process.hrtime.bigint(), { encoding: 'utf8' });
+test('restart', (done) => {
+  events = lib({
+    executable: `${appFile} ${incrementFile}`,
+    watchDir: pathResolve(__dirname, '../../tmp'),
+    // debug: true,
+  });
 
-//   expect(getValue(incrementFile)).toEqual(2);
-// });
+  events.on('started', () => {
+    console.log('started lib...');
+    if (!isStarted) {
+      isStarted = true;
+
+      // Even though process is running, wait a bit for it to initialize properly
+      setTimeout(() => {
+        console.log('touching file...');
+        writeFileSync(touchFile, process.hrtime.bigint(), { encoding: 'utf8' });
+      }, 100);
+    } else {
+      // Even though process is running, wait a bit for it to initialize properly
+      setTimeout(() => {
+        console.log('workfile value:', getValue(incrementFile));
+
+        expect(getValue(incrementFile)).toEqual(1);
+
+        setTimeout(done, 100);
+      }, 100);
+    }
+  });
+}, 10 * 1000);
+
+afterAll(() => {
+  events.emit('kill');
+});
