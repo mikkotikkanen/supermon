d169c5db9389642761a6a20e134d14c86005f79e
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2020-06-19 16:49:37 +0300

    cleaned watcher

--- src/lib/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-export default (func: Function, delay: number): Function => {
-  let inDebounce: NodeJS.Timeout;
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return function callee(...args: any[]): void {
-    clearTimeout(inDebounce);
-    inDebounce = setTimeout(() => func(...args), delay);
-  };
-};
