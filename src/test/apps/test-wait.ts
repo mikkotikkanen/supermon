c55f4c337c8b4fb6afbddb781d95c162179c971e
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-13 20:16:54 +0200

    Test debugging

--- src/test/apps/test-wait.ts
+++ src/test/apps/test-wait.ts
@@ -11,7 +11,10 @@
  */
 const arg = process.argv.pop() as string;
 const waitInSeconds = parseInt(arg, 10) || 1; // Default to 1 second
+let temp = 1;
 
 setTimeout(() => {
   // Keep the app running for set time
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  temp += 1;
 }, waitInSeconds * 1000);
