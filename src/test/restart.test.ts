34d7d5db2fac1085ec7c9ec93ab52b1b19279f97
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-13 14:19:18 +0200

    Fixed restart test to use events and wait app

--- src/test/restart.test.ts
+++ src/test/restart.test.ts
@@ -1,55 +1,40 @@
-/* eslint-disable jest/no-done-callback */
-/* eslint-disable jest/no-conditional-expect */
 import { writeFileSync } from 'fs';
 import { join, resolve as pathResolve } from 'path';
-import { clean, getValue } from './apps/libs/incrementer';
 import getWorkDir from './lib/getWorkDir';
 import lib from '../lib';
 import LibEventBus from '../lib/LibEventBus';
 
 const workDir = getWorkDir();
-const appFile = join(workDir, '..', 'dist', 'test', 'apps', 'test-incrementer.js');
-const incrementFile = 'restart_incrementer';
 const touchFile = join(workDir, 'empty_touch_file.js');
-let isStarted = false;
-
 
 let eventBus: LibEventBus;
 
-beforeAll(() => {
-  clean(incrementFile);
-});
+test('Application should restart on file change', () => new Promise<void>((resolve) => {
+  // For some reason test doesn't work with .ts file (executed by ts-node)
+  const appFile = join(workDir, '..', 'dist', 'test', 'apps', 'test-wait.js');
 
-test('restart', (done) => {
   eventBus = lib({
-    executable: `${appFile} ${incrementFile}`,
+    executable: `${appFile} 1`,
     watchdir: pathResolve(__dirname, '../../tmp'),
-    // debug: true,
+    delay: 10,
     logging: false,
+    // debug: true,
   });
 
   eventBus.on(eventBus.Events.Started, () => {
-    if (!isStarted) {
-      isStarted = true;
-
-      expect(getValue(incrementFile)).toEqual(0);
-
-      // Wait a bit to make sure watcher is initialized
-      setTimeout(() => {
-        writeFileSync(touchFile, `${process.hrtime.bigint()}`, { encoding: 'utf8' });
-      }, 100);
-    } else {
-      // Wait a bit to make sure incrementer has written the file
-      setTimeout(() => {
-        expect(getValue(incrementFile)).toEqual(2);
-
-        // setTimeout(done, 100);
-        done();
-      }, 100);
-    }
+    // Wait a bit to make sure watcher is initialized
+    setTimeout(() => {
+      writeFileSync(touchFile, `${process.hrtime.bigint()}`, { encoding: 'utf8' });
+    }, 100);
   });
-}, 10 * 1000);
+
+  eventBus.on(eventBus.Events.Restarted, () => {
+    expect(true).toBeTruthy();
+    resolve();
+  });
+}));
 
 afterAll(() => {
+  // Make sure the process is killed
   eventBus.kill();
 });
