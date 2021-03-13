21fec485841d5eace41bd7bc61bc26aeb3cd0ce6
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-13 14:18:59 +0200

    Added test app that just waits

---
+++ src/test/apps/test-wait.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/**
+ * Application for testing purposes
+ *
+ * Only waits for given amount of time
+ *
+ * Wait time can be defined as an cmd argument
+ * (fe. wait for 5 seconds `ts-node test-wait.ts 5`)
+ *
+ * Defaults to 1 second wait time
+ */
+const arg = process.argv.pop() as string;
+const waitInSeconds = parseInt(arg, 10) || 1; // Default to 1 second
+
+setTimeout(() => {
+  // Keep the app running for set time
+}, waitInSeconds * 1000);
