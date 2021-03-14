b97c4187b61a235b090db75a6b02267514168dd1
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-14 21:10:53 +0200

    Basic event bus stub

---
+++ src/lib/EventBus/index.ts
@@ -0,0 +1,54 @@
+import EventEmitter from 'events';
+
+export type EventBusProps = {
+  /**
+   * Log events out
+   */
+  debug: boolean;
+}
+
+
+export enum ChildEvents {
+  Start = 'ChildStart',
+  Started = 'ChildStarted',
+  Restart = 'ChildRestart',
+  Restarted = 'ChildRestarted',
+  Stop = 'ChildStop',
+  Stopped = 'ChildStopped',
+}
+
+export enum InstallEvents {
+  Install = 'Install',
+  Installing = 'Installing',
+  Installed = 'Installed',
+}
+
+export enum WatchEvents {
+  Start = 'WatchStart',
+  FilesChanged = 'WatchFilesChanged',
+  Enable = 'WatchEnable',
+  Disable = 'WatchDisable',
+  Stop = 'WatchStop',
+  Stopped = 'WatchStopped',
+}
+
+
+export default class EventBus extends EventEmitter {
+  private debug = false;
+
+  constructor({
+    debug = false,
+  }: EventBusProps) {
+    super();
+    this.debug = debug;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  emit(event: string, ...args: any[]): boolean {
+    if (this.debug) {
+      console.log(`Event "${event}" triggered.`);
+    }
+
+    return super.emit(event, ...args);
+  }
+}
