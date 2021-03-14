ec9b790f46b6ec045ed43376656042ff5029c9f9
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-14 18:41:34 +0200

    Removed unused files

--- src/test/apps/test-incrementer.ts
+++
@@ -1,8 +0,0 @@
-import { incrementer } from './libs/incrementer';
-
-const workfile = process.argv.pop() as string;
-incrementer(workfile);
-
-setTimeout(() => {
-  // Keep the app running for few seconds
-}, 10 * 1000);
