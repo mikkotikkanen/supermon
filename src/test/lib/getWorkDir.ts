c21c53052c02034babaa24d53eccd10afa1d4533
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-14 18:18:50 +0200

    Added cleanup and error handling

--- src/test/lib/getWorkDir.ts
+++ src/test/lib/getWorkDir.ts
@@ -1,11 +1,18 @@
-import { existsSync, mkdirSync } from 'fs';
+import { existsSync, mkdirSync, rmdirSync } from 'fs';
 import { join } from 'path';
 
 const dirPath = join(__dirname, '..', '..', '..', 'tmp');
 
 export default (): string => {
-  if (!existsSync(dirPath)) {
+  try {
+    if (existsSync(dirPath)) {
+      rmdirSync(dirPath, { recursive: true });
+    }
     mkdirSync(dirPath);
+  } catch (err) {
+    // fs operations may fail silently
+    console.log(err);
+    throw err;
   }
 
   return dirPath;
