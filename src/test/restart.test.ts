d406b59b35dd9b46656d7f7ae2694842230900e0
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-14 18:19:41 +0200

    Added creation of the touch file in beforeAll

--- src/test/restart.test.ts
+++ src/test/restart.test.ts
@@ -1,21 +1,31 @@
 import { writeFileSync } from 'fs';
-import { join, resolve as pathResolve } from 'path';
+import { join } from 'path';
 import getWorkDir from './lib/getWorkDir';
 import lib from '../lib';
 import LibEventBus from '../lib/LibEventBus';
 
-const workDir = getWorkDir();
-const touchFile = join(workDir, 'empty_touch_file.js');
-
+let workDir: string;
+let appFile: string;
+let touchFile: string;
 let eventBus: LibEventBus;
+
+beforeAll(() => {
+  workDir = getWorkDir();
+  appFile = join(workDir, '..', 'dist', 'test', 'apps', 'test-wait.js');
+  touchFile = join(workDir, 'empty_touch_file.js');
+
+  // Create the touchFile so that file watcher has something to watch
+  writeFileSync(touchFile, '0', { encoding: 'utf8' });
+});
 
 test('Application should restart on file change', () => new Promise<void>((resolve) => {
   // For some reason test doesn't work with .ts file (executed by ts-node)
-  const appFile = join(workDir, '..', 'dist', 'test', 'apps', 'test-wait.js');
+  appFile = join(workDir, '..', 'dist', 'test', 'apps', 'test-wait.js');
+  touchFile = join(workDir, 'empty_touch_file.js');
 
   eventBus = lib({
-    executable: `${appFile} 5`,
-    watchdir: pathResolve(__dirname, '../../tmp'),
+    executable: `${appFile} 1`,
+    watchdir: workDir,
     delay: 10,
     logging: false,
     firstRunSync: false,
@@ -24,17 +34,13 @@ test('Application should restart on file change', () => new Promise<void>((resol
   });
 
   eventBus.on(eventBus.Events.Started, () => {
-    console.log('started...');
     // Wait a bit to make sure watcher is initialized
     setTimeout(() => {
-      console.log(`touching (${touchFile})...`);
-      // writeFileSync(touchFile, `${process.hrtime.bigint()}`, { encoding: 'utf8' });
-      writeFileSync(touchFile, `${Math.random() * 10000}`, { encoding: 'utf8' });
+      writeFileSync(touchFile, `${process.hrtime.bigint()}`, { encoding: 'utf8' });
     }, 100);
   });
 
   eventBus.on(eventBus.Events.Restarted, () => {
-    console.log('restarting...');
     expect(true).toBeTruthy();
     resolve();
   });
