06116a7b7470e7e7dcdcc741fd2b10d4911969ab
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-16 20:19:28 +0200

    Clearer event naming

--- src/lib/EventBus/index.ts
+++ src/lib/EventBus/index.ts
@@ -9,27 +9,27 @@ export type EventBusProps = {
 
 
 export enum ChildEvents {
-  Start = 'ChildStart',
-  Started = 'ChildStarted',
-  Restart = 'ChildRestart',
-  Restarted = 'ChildRestarted',
-  Stop = 'ChildStop',
-  Stopped = 'ChildStopped',
+  Start = 'CHILD_START',
+  Started = 'CHILD_STARTED',
+  Restart = 'CHILD_RESTART',
+  Restarted = 'CHILD_RESTARTED',
+  Stop = 'CHILD_STOP',
+  Stopped = 'CHILD_STOPPED',
 }
 
 export enum InstallEvents {
-  Install = 'Install',
-  Installing = 'Installing',
-  Installed = 'Installed',
+  Install = 'INSTALL_INSTALL',
+  Installing = 'INSTALL_INSTALLING',
+  Installed = 'INSTALL_INSTALLED',
 }
 
 export enum WatchEvents {
-  Start = 'WatchStart',
-  FilesChanged = 'WatchFilesChanged',
-  Enable = 'WatchEnable',
-  Disable = 'WatchDisable',
-  Stop = 'WatchStop',
-  Stopped = 'WatchStopped',
+  Start = 'WATCH_START',
+  FilesChanged = 'WATCH_FILESCHANGED',
+  Enable = 'WATCH_ENABLE',
+  Disable = 'WATCH_DISABLE',
+  Stop = 'WATCH_STOP',
+  Stopped = 'WATCH_STOPPED',
 }
 
 
