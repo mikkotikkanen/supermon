cbcc716da137c23785bafae2c96168502ec0bd00
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-03-29 14:16:44 +0300

    Set dependencies as optional

--- src/lib/modules/types/PackageJSON.ts
+++ src/lib/modules/types/PackageJSON.ts
@@ -3,7 +3,7 @@ import { DependencyMap } from './DependencyMap';
 export interface PackageJSON extends Object {
   readonly name: string;
   readonly version: string;
-  readonly dependencies: DependencyMap;
-  readonly devDependencies: DependencyMap;
-  readonly peerDependencies: DependencyMap;
+  readonly dependencies?: DependencyMap;
+  readonly devDependencies?: DependencyMap;
+  readonly peerDependencies?: DependencyMap;
 }
