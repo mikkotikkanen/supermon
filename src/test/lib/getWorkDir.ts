715c1b5051058285a89c1947e6a5b5e2dc4ba020
Mikko Tikkanen <mikko.tikkanen@aller.com>
2021-07-04 15:16:44 +0300

    Switched to rimraf and added types

--- src/test/lib/getWorkDir.ts
+++ src/test/lib/getWorkDir.ts
@@ -1,12 +1,14 @@
 import { existsSync, mkdirSync, rmdirSync } from 'fs';
 import { join } from 'path';
+import rimraf from 'rimraf';
 
 const dirPath = join(__dirname, '..', '..', '..', 'tmp');
 
 export default (): string => {
   try {
     if (existsSync(dirPath)) {
-      rmdirSync(dirPath, { recursive: true });
+      // rmdirSync(dirPath, { recursive: true });
+      rimraf.sync(dirPath);
     }
     mkdirSync(dirPath);
   } catch (err) {
